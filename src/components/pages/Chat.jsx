436c81d4431d0db59172d9456a4502be46ab0146
MateuszPuto <mateuszmputo@gmail.com>
2023-04-26 16:23:29 +0200

    minor chat improvement

--- src/components/pages/Chat.jsx
+++ src/components/pages/Chat.jsx
@@ -1,25 +1,39 @@
 import { firebaseApp } from "../../firebase";
 import { doc, getFirestore } from "firebase/firestore";
-import { useDocumentData } from "react-firebase-hooks/firestore";
+import { useDocumentData, useDocumentDataOnce } from "react-firebase-hooks/firestore";
+import { useState } from 'react';
 
-const ViewChat = () => {  
-    const [data, loading, error] = useDocumentData(
-      doc(getFirestore(firebaseApp), "chats", "rgtDBkgeISYUrolySxv2"));
+const ViewChat = () => {
+    const [username, setUsername] = useState("");
+
+    const [data_chats, loading, error] = useDocumentData(
+        doc(getFirestore(firebaseApp), "chats", "2awCS7fkdLnIbZSYe3j3"));
+
+    if (data_chats == undefined) {
+        return(<div></div>);
+    } else {
+        const user = data_chats.members[0];
+        const username = GetUser(user); 
+
+        const messages = data_chats.messages.map((message) => <li class='list-group-item'><b>{username}:</b> {message.content}</li>);
         
-    if(data == undefined) {
-        return <div></div>;
-    }
-    else {
-        const messages = data.messages.map((message) => <li>{message}</li>);
+        const styling = {
+            width: 300,
+        };
 
-        return (<div>
-            {data.user_id}
-            {data.second_user_id}
-            <ul>
+        return (<div class="card" style={styling}>
+            <ul class='list-group list-group-flush'>
                 {messages}
             </ul>
             </div>);
     }
+};
+
+const GetUser = (user) => {
+    // const [data, loading, error] = useDocumentData(
+    //     doc(getFirestore(firebaseApp), "users", user));
+
+    return "tom";
 };
 
 export default ViewChat;
