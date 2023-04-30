621663862eccc7c8802a3725a83476c7dd817fda
Michał Szczurek <michalszczurek794@gmail.com>
2023-04-30 18:22:14 +0200

    fit profile info on screen

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -5,29 +5,25 @@ const ProfileInfo = (
   { name, email, phoneNumber, photoUrl, showNull } = { showNull: false }
 ) => {
   return (
-    <div className="d-flex">
-      <div className="me-3">
-        <img
-          className="border rounded mt-3"
-          src={photoUrl || NO_PHOTO_URL}
-          height={100}
-        />
-      </div>
-      <div>
-        <h3 className="mt-3">{name}</h3>
-        {(email || showNull) && (
-          <div className="d-flex align-items-center mt-2">
-            <img src="/mail-icon.svg" height={20} className="me-2" />
-            {email || labels.NOT_SET}
-          </div>
-        )}
-        {(phoneNumber || showNull) && (
-          <div className="d-flex align-items-center mt-2">
-            <img src="/phone-icon.svg" height={20} className="me-2" />
-            {phoneNumber || labels.NOT_SET}
-          </div>
-        )}
-      </div>
+    <div>
+      <h3 className="mt-3">{name}</h3>
+      <img
+        className="border rounded"
+        src={photoUrl || NO_PHOTO_URL}
+        height={100}
+      />
+      {(email || showNull) && (
+        <div className="d-flex align-items-center mt-2">
+          <img src="/mail-icon.svg" height={20} className="me-2" />
+          {email || labels.NOT_SET}
+        </div>
+      )}
+      {(phoneNumber || showNull) && (
+        <div className="d-flex align-items-center mt-2">
+          <img src="/phone-icon.svg" height={20} className="me-2" />
+          {phoneNumber || labels.NOT_SET}
+        </div>
+      )}
     </div>
   );
 };
